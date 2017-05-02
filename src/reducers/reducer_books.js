d0c1a3c5b8acb1b68395323453a6faa93c3f05f2
Eric Van Dyn Hoven <eric.vandynhoven@gmail.com>
2017-05-01 21:47:56 -0700

    added book reducer and container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The Goods Parts'},
+    {title: 'Harry Potter'},
+    {title: 'The Fellowship of the Ring'},
+    {title: 'Eloquent Ruby'},
+  ]
+}
